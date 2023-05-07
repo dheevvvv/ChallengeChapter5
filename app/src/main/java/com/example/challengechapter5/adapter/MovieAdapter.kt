6d08485ef2196e3540fab90ffd99db45796607ee
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-05-08 01:26:12 +0700

    adding movie adapter

---
+++ app/src/main/java/com/example/challengechapter5/adapter/MovieAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.challengechapter5.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.challengechapter5.databinding.ItemMovieBinding
+import com.example.challengechapter5.model.ResultPopular
+
+class MovieAdapter(var listMovie:List<ResultPopular>): RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+    var onClick: ((ResultPopular)->Unit)? = null
+    class ViewHolder(var binding: ItemMovieBinding):RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listMovie.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val list =listMovie[position]
+        holder.binding.titleMovie.text = list.title
+        Glide.with(holder.itemView).load("https://image.tmdb.org/t/p/w400${list.posterPath}")
+            .into(holder.binding.ivMovie)
+        holder.binding.areaDetail.setOnClickListener{
+            onClick?.invoke(list)
+        }
+        holder.binding.rateMovie.text = list.voteAverage.toString()
+    }
+}
